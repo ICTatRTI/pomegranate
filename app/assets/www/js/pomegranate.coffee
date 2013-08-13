608bbbe4843cd66e4c7cf138808868a881be4b46
mikeymckay <mikeymckay@gmail.com>
2013-08-12 23:29:13 -0400

    Untested but mostly all implemented.

--- app/assets/www/js/pomegranate.coffee
+++ app/assets/www/js/pomegranate.coffee
@@ -1,19 +1,58 @@
 $ -> # document ready
   $(document).on "deviceready", -> # phonegap deviceready
 
+    cordova.execute = (options) ->
+      cordova.exec options.success, options.error , options.plugin.name, options.plugin.function, options.plugin.args
+
     db = new PouchDB("Pomegranate")
-# Check for most recent sms
+
+    # Views
+    #
     smsByTimeReceived = (doc) ->
       if doc.time_received
         emit(doc.time_received, null)
 
+    smsReceivedButNotProcessed = (doc) ->
+      if doc.time_received and not doc.processed
+        emit(doc.time_received, null)
+
+    smsToSend = (doc) ->
+      if doc.time_created and not doc.processed
+        emit(doc.time_created, null)
+
+    # Utility functions
     saveResults = (results) ->
       _(results.texts).each (text) ->
-        db.post text
+        # Make this deterministic to allow for syncing after deleting the database locally (and syncing with cloud)
+        text._id = "#{text.time_received}+#{text.from}"
+        db.put text
+
+    getAllSmsSaveInDB = ->
+      cordova.execute
+        success: saveResults
+        error: (error) ->
+          console.log "Error while saving: #{JSON.stringify error}"
+        plugin:
+          name: "ReadSms"
+          function: "GetTexts"
+          args: ["",-1]
+
+    getAllSmsAfterCutoffSaveInDB = (cutoff) ->
+      #cordova.exec saveResults, error , "ReadSms", "GetTextsAfter", ["", response.rows[0].time_received, -1] # No number specified, -1 is unlimited
+      cordova.execute
+        success: saveResults
+        error: (error) ->
+          console.log "Error while saving: #{JSON.stringify error}"
+        plugin:
+          name: "ReadSms"
+          function: "GetTextsAfter"
+          args: ["", cutoff, -1]
 
     error = (error) ->
       console.log "Error: #{JSON.stringify error}"
 
+    # Check the db for the most recent SMS
+    # Then add any new ones on the phone to it
     db.query
       map: smsByTimeReceived
       {
@@ -23,52 +62,42 @@ $ -> # document ready
         descending: true
       }
       (error, response) ->
-        console.log "error: #{JSON.stringify error}" if error
-        console.log "response: #{JSON.stringify response}"
+        
         if response.rows.length is 0
           console.log "No SMS's in DB, loading all SMSs on phone"
-          cordova.exec saveResults, error , "ReadSms", "GetTexts", ["", -1] # No number specified, -1 is unlimited
+          getAllSmsSaveInDB()
         else
-          cordova.exec saveResults, error , "ReadSms", "GetTextsAfter", ["", response.rows[0].time_received, -1] # No number specified, -1 is unlimited
+          console.log "Getting messages after #{response.rows[0].time_received} and adding them to DB"
+          getAllSmsAfterCutoffSaveInDB(response.rows[0].time_received)
 
-
+    db.changes
+#      since: 20
+#      TODO figure out how to use since - sequence numbers?
+      continuous: true
+      include_docs: true
+      filter: smsToSend
+      onChange: (doc) ->
+        console.log "Change: #{doc}"
+        cordova.execute
+          success: ->
+            console.log "SMS sent: #{JSON.stringify doc}"
+            doc.processed = true
+            db.put doc
+          error: (error) ->
+            console.log "Error on sending SMS: #{JSON.stringify doc}, error: #{JSON.stringify error}"
+          plugin:
+            name: "SmsPlugin"
+            function: "SendSMS"
+            args: [doc.to, doc.message]
 
     $("#send").click ->
-      number = $("#phone_number").val()
-      message = $("#message").val()
+      doc.to = $("#to")
+      doc.message = $("#message")
+      doc.time_created = Date.now()
+      doc._id = "#{doc.time_received}+#{text.to}"
+      db.put doc # should trigger changes callback
 
-      success = -> alert "Message sent successfully"
-      error = (e) -> alert "Message Failed:" + e
-
-      cordova.exec success, error, "SmsPlugin", "SendSMS", [number, message]
-
-    
-    updateMessageTable = (results) ->
-      $("#messages").html "
-        <table>
-          <thead>
-            <th>from</th>
-            <th>message</th>
-            <th>date</th>
-          </thead>
-          <tbody>
-            #{
-              _(results.texts).map( (text) ->
-                "
-                  <tr>
-                    <td>#{text.from}</td>
-                    <td>#{text.message}</td>
-                    <td>#{text.time_received}</td>
-                  </tr>
-                "
-              ).join("")
-            }
-          </tbody>
-        </table>
-      "
-
-    error = (error) ->
-      console.log "THERE WAS AN ERROR!!! aaaaaAAAAHHHHH!!!"
-      console.log error
-
-    cordova.exec updateMessageTable, error, "ReadSms", "GetTexts", ["", -1] # No number specified, -1 is unlimited
+    PouchDB.replicate 'pomegranate', 'http://mikeymckay.iriscouch.com/pomegranate',
+      continuous: true
+    PouchDB.replicate 'http://mikeymckay.iriscouch.com/pomegranate','pomegranate',
+      continuous: true
--- app/assets/www/js/pomegranate.js
+++ app/assets/www/js/pomegranate.js
@@ -2,17 +2,57 @@
 (function() {
   $(function() {
     return $(document).on("deviceready", function() {
-      var db, error, saveResults, smsByTimeReceived, updateMessageTable;
+      var db, error, getAllSmsAfterCutoffSaveInDB, getAllSmsSaveInDB, saveResults, smsByTimeReceived, smsReceivedButNotProcessed, smsToSend;
 
+      cordova.execute = function(options) {
+        return cordova.exec(options.success, options.error, options.plugin.name, options.plugin["function"], options.plugin.args);
+      };
       db = new PouchDB("Pomegranate");
       smsByTimeReceived = function(doc) {
         if (doc.time_received) {
           return emit(doc.time_received, null);
         }
       };
+      smsReceivedButNotProcessed = function(doc) {
+        if (doc.time_received && !doc.processed) {
+          return emit(doc.time_received, null);
+        }
+      };
+      smsToSend = function(doc) {
+        if (doc.time_created && !doc.processed) {
+          return emit(doc.time_created, null);
+        }
+      };
       saveResults = function(results) {
         return _(results.texts).each(function(text) {
-          return db.post(text);
+          text._id = "" + text.time_received + "+" + text.from;
+          return db.put(text);
+        });
+      };
+      getAllSmsSaveInDB = function() {
+        return cordova.execute({
+          success: saveResults,
+          error: function(error) {
+            return console.log("Error while saving: " + (JSON.stringify(error)));
+          },
+          plugin: {
+            name: "ReadSms",
+            "function": "GetTexts",
+            args: ["", -1]
+          }
+        });
+      };
+      getAllSmsAfterCutoffSaveInDB = function(cutoff) {
+        return cordova.execute({
+          success: saveResults,
+          error: function(error) {
+            return console.log("Error while saving: " + (JSON.stringify(error)));
+          },
+          plugin: {
+            name: "ReadSms",
+            "function": "GetTextsAfter",
+            args: ["", cutoff, -1]
+          }
         });
       };
       error = function(error) {
@@ -26,40 +66,50 @@
         include_docs: true,
         descending: true
       }, function(error, response) {
-        if (error) {
-          console.log("error: " + (JSON.stringify(error)));
-        }
-        console.log("response: " + (JSON.stringify(response)));
         if (response.rows.length === 0) {
           console.log("No SMS's in DB, loading all SMSs on phone");
-          return cordova.exec(saveResults, error, "ReadSms", "GetTexts", ["", -1]);
+          return getAllSmsSaveInDB();
         } else {
-          return cordova.exec(saveResults, error, "ReadSms", "GetTextsAfter", ["", response.rows[0].time_received, -1]);
+          console.log("Getting messages after " + response.rows[0].time_received + " and adding them to DB");
+          return getAllSmsAfterCutoffSaveInDB(response.rows[0].time_received);
+        }
+      });
+      db.changes({
+        continuous: true,
+        include_docs: true,
+        filter: smsToSend,
+        onChange: function(doc) {
+          console.log("Change: " + doc);
+          return cordova.execute({
+            success: function() {
+              console.log("SMS sent: " + (JSON.stringify(doc)));
+              doc.processed = true;
+              return db.put(doc);
+            },
+            error: function(error) {
+              return console.log("Error on sending SMS: " + (JSON.stringify(doc)) + ", error: " + (JSON.stringify(error)));
+            },
+            plugin: {
+              name: "SmsPlugin",
+              "function": "SendSMS",
+              args: [doc.to, doc.message]
+            }
+          });
         }
       });
       $("#send").click(function() {
-        var message, number, success;
-
-        number = $("#phone_number").val();
-        message = $("#message").val();
-        success = function() {
-          return alert("Message sent successfully");
-        };
-        error = function(e) {
-          return alert("Message Failed:" + e);
-        };
-        return cordova.exec(success, error, "SmsPlugin", "SendSMS", [number, message]);
+        doc.to = $("#to");
+        doc.message = $("#message");
+        doc.time_created = Date.now();
+        doc._id = "" + doc.time_received + "+" + text.to;
+        return db.put(doc);
       });
-      updateMessageTable = function(results) {
-        return $("#messages").html("        <table>          <thead>            <th>from</th>            <th>message</th>            <th>date</th>          </thead>          <tbody>            " + (_(results.texts).map(function(text) {
-          return "                  <tr>                    <td>" + text.from + "</td>                    <td>" + text.message + "</td>                    <td>" + text.time_received + "</td>                  </tr>                ";
-        }).join("")) + "          </tbody>        </table>      ");
-      };
-      error = function(error) {
-        console.log("THERE WAS AN ERROR!!! aaaaaAAAAHHHHH!!!");
-        return console.log(error);
-      };
-      return cordova.exec(updateMessageTable, error, "ReadSms", "GetTexts", ["", -1]);
+      PouchDB.replicate('pomegranate', 'http://mikeymckay.iriscouch.com/pomegranate', {
+        continuous: true
+      });
+      return PouchDB.replicate('http://mikeymckay.iriscouch.com/pomegranate', 'pomegranate', {
+        continuous: true
+      });
     });
   });
 
